7db2f47702da9799d1d8d6c614f14b3e564f7e38
Shawn Medero <soypunk@users.noreply.github.com>
2020-03-20 10:42:40 -0700

    finished in media res table

--- src/zozer-solo.js
+++ src/zozer-solo.js
@@ -1009,42 +1009,29 @@ const ZozerSolo = (() => {
    }
    
    starting_situation() {
-   /*
-2D,Starting Situation
-2-3,Hijack. The free trader your PCs are travelling on has been hijacked by three of the
-"",passengers (possibly with help from a crew-member – it is still unclear). One hijacker
-"","has barricaded himself into the engineering section, the others are on the bridge. All"
-"",the crewmembers (including the passengers – the PCs) have been locked inside a
-"",single stateroom. The crew have no combat experience and are fearful of being
-"",spaced. What do you do?
-4,"Manslaughter. It was a simple job, provide protection and company for a rich"
-"",traveller to this backwater world. But he’s gotten into a fight that wasn’t his fault and
-"","accidentally killed someone. Now the heat is on, be it the cops, a local gang or"
-"",whatever. Get to the starport and off-world – fast.
-5,Pirates. The free trader your PCs are travelling on has been intercepted by a scout
-"",ship that demands to dock otherwise it will launch a full salvo of missiles. Travelling
-"","only between ‘safe’ systems, the free trader is unarmed. The crew have no combat"
-"",experience and are about to be boarded in 15 minutes. What do you do?
-5,AWOL. The PCs are being paid to get a military officer to the starport where a ship is
-"","waiting to take him off-world. He may be a defector, a spy, a coward or simply be"
-"","disillusioned. The military of the planet do not intend him to leave, however."
-6,"The Package. Paid to carry a small package off-world, the PCs leave the patron’s"
-"",premises just before gunmen arrive to kill him. Now they want the package and will kill
-"",anyone who has touched it. Get off world quickly!
-7,Stuck. There is an emergency on planet that the PCs are caught up in. Travel is
-"",curtailed and if they don’t get to the starport in 3 days the last transports will leave and
-"",there will be no way off planet for weeks or months.
-8-9,Arrested. One of the PCs has been held by security at the hotel for some (real or
-"",imagined) past crime until the police arrive. The hotel security officer has no idea the
-"",suspect has friends in the building ...
-10-11,"Missing. One of the PCs (choose or determine randomly) is missing, despite the"
-"",group having booked passage on a starliner leaving in three days’ time. The reason
-"",for the disappearance should be linked to one of the PC’s hooks if possible.
-12,Low Berth. Low berth pods open automatically and the PCs get out. They are on a
-"","ship, but there seems to be no crew onboard. What type of ship is it? Where are"
-"","they? What happened? The SOLO Player might decide between alien infestation,"
-"","piracy or hijacking, or some strange jump drive anomaly."
-   */
+   
+      var starting_situation_table = {
+         2: "Hijack. The free trader your PCs are travelling on has been hijacked by three of the passengers (possibly with help from a crew-member – it is still unclear). One hijacker has barricaded himself into the engineering section, the others are on the bridge. All the crewmembers (including the passengers – the PCs) have been locked inside a single stateroom. The crew have no combat experience and are fearful of being spaced. What do you do?",
+         3: "Manslaughter. It was a simple job, provide protection and company for a rich traveller to this backwater world. But he’s gotten into a fight that wasn’t his fault and accidentally killed someone. Now the heat is on, be it the cops, a local gang or whatever. Get to the starport and off-world – fast.",
+         4: "Pirates. The free trader your PCs are travelling on has been intercepted by a scout ship that demands to dock otherwise it will launch a full salvo of missiles. Travelling only between ‘safe’ systems, the free trader is unarmed. The crew have no combat experience and are about to be boarded in 15 minutes. What do you do?",
+         5: "AWOL. The PCs are being paid to get a military officer to the starport where a ship is waiting to take him off-world. He may be a defector, a spy, a coward or simply be disillusioned. The military of the planet do not intend him to leave, however.",
+         6: "The Package. Paid to carry a small package off-world, the PCs leave the patron’s premises just before gunmen arrive to kill him. Now they want the package and will kill anyone who has touched it. Get off world quickly!",
+         7: "Stuck. There is an emergency on planet that the PCs are caught up in. Travel is curtailed and if they don’t get to the starport in 3 days the last transports will leave and there will be no way off planet for weeks or months."
+         8: "Arrested. One of the PCs has been held by security at the hotel for some (real or imagined) past crime until the police arrive. The hotel security officer has no idea the suspect has friends in the building ..."
+        10: "Missing. One of the PCs (choose or determine randomly) is missing, despite the group having booked passage on a starliner leaving in three days’ time. The reason for the disappearance should be linked to one of the PC’s hooks if possible.",
+        12: "Low Berth. Low berth pods open automatically and the PCs get out. They are on a ship, but there seems to be no crew onboard. What type of ship is it? Where are they? What happened? The SOLO Player might decide between alien infestation, piracy or hijacking, or some strange jump drive anomaly." 
+      }
+      
+      var roller = new DiceRoller();
+      var rolls = roller.roll('2d6');
+      var result = starting_situation_tableutils.getClosestKey(starting_situation_table, rolls.total)];
+
+      return {
+         'rolls': rolls,
+         'total': rolls.total,
+         'result': result,
+         'extras': false
+      }      
    }
    
    patron() {
